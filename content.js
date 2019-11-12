91165caad25ef2ac83fd085ecd82abe1a16ecd9e
mastinoz <persiste1@gmail.com>
2019-11-12 04:27:28 +0100

    Scroll Down the Page 82px

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 // Verify if YouTube Watch page
 let isValidHostname = /youtube.com\/watch\\?/.test(window.location);
 if (isValidHostname) {
-    
+
+    // Auto Scroll Down Page 82px
+    document.documentElement.scrollTop = document.body.scrollTop = 82;
+
 }
